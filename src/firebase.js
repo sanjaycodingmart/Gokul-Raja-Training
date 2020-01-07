00f2fffbb7993e98b5954553cfa91ebd932efd4e
tsgokulraja <gokulraja.sunairaja@codingmart.com>
2020-01-07 14:48:29 +0530

    Removed Firebase Credentials

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 const firebaseConfig = {
-    apiKey: "AIzaSyAGaeofETSA4twzRWpaQIuNlScz8oIAfEY",
-    authDomain: "mart-sheets.firebaseapp.com",
-    databaseURL: "https://mart-sheets.firebaseio.com",
-    projectId: "mart-sheets",
-    storageBucket: "mart-sheets.appspot.com",
-    messagingSenderId: "214542388927",
-    appId: "1:214542388927:web:a5278761f3c66db2ea800a",
-    measurementId: "G-JGV0QBGGJP"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "  ",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
   firebase.initializeApp(firebaseConfig);
 
